3a480a3894af3ccdf90c959d6f15d0f896b35e9c
zacscodingclub <zbaston@gmail.com>
2016-09-12 09:57:27 -0400

    add Create, Update, and Delete functionality to Article model

--- models/article.js
+++ models/article.js
@@ -38,3 +38,33 @@ module.exports.getCategoryArticles = function(category, callback) {
   var query = { category: category }
   Article.find(query, callback);
 }
+
+module.exports.createArticle = function(data, callback) {
+  data.save(callback);
+}
+
+module.exports.updateArticle = function(id, data, callback) {
+  var title = data.title;
+  var body = data.body;
+  var category = data.category;
+  var url = data.url;
+
+  var query = { _id: id };
+  Article.findById(query, function(err, article) {
+    if(!article) {
+      return next(new Error('Could not load article.')) ;
+    } else {
+      article.title = title;
+      article.body = body;
+      article.category = category;
+      article.url = url;
+
+      article.save(callback);
+    }
+  });
+}
+
+module.exports.deleteArticle = function(id, callback) {
+  var query = { _id: id };
+  Article.find(query).remove(callback);
+}

9f43469e48198d15d4ff4d2552124a2d346979eb
zacscodingclub <zbaston@gmail.com>
2016-09-12 17:37:15 -0400

    build controller for article show action

--- public/javascripts/controllers/articles.controller.js
+++ public/javascripts/controllers/articles.controller.js
@@ -3,4 +3,18 @@ angular.module('toRead')
     $http.get('/articles').success(function(data) {
       $scope.articles = data;
     })
-  }]);
+  }])
+
+  .controller('ArticlesCategoryController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('/articles/category/' + $routeParams.category_name).success(function(data) {
+      $scope.categoryArticles = data;
+
+      $scope.category = $routeParams.category_name;
+    })
+  }])
+
+  .controller('ArticleShowController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('/articles/' + $routeParams.id ).success(function(data) {
+      $scope.article = data;
+    })
+  }])

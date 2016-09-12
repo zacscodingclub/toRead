dc9779df6a3a70cf068201fb66255cda4d38e7a8
zacscodingclub <zbaston@gmail.com>
2016-09-12 18:43:17 -0400

    basic working app complete with Articles CRUD functionality

--- public/javascripts/controllers/articles.controller.js
+++ public/javascripts/controllers/articles.controller.js
@@ -13,8 +13,63 @@ angular.module('toRead')
     })
   }])
 
-  .controller('ArticleShowController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  .controller('ArticleShowController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
+    $http.get('/articles/' + $routeParams.id ).success(function(data) {
+      $scope.article = data;
+    });
+
+    $scope.deleteArticle = function() {
+      var deletePath = '/articles/' + $routeParams.id;
+      
+      $http.delete(deletePath).success(function(data) {
+        console.log(data);
+      });
+
+      $location.path('/');
+    }
+
+  }])
+
+  .controller('NewArticleController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
+    $http.get('/categories').success(function(data) {
+      $scope.categories = data;
+    })
+
+    $scope.addArticle = function() {
+      var data = {
+        title: $scope.title,
+        body: $scope.body,
+        category: $scope.category
+      }
+
+      $http.post('/articles', data).success(function(data, status) {
+        console.log(status);
+      });
+
+      $location.path('/articles');
+    }
+  }])
+
+  .controller('EditArticleController', ['$scope', '$http', '$routeParams', '$location', function($scope, $http, $routeParams, $location) {
     $http.get('/articles/' + $routeParams.id ).success(function(data) {
       $scope.article = data;
     })
+    $http.get('/categories').success(function(data) {
+      $scope.categories = data;
+    })
+
+    $scope.updateArticle = function() {
+      var data = {
+        id: $routeParams.id,
+        title: $scope.article.title,
+        body: $scope.article.body,
+        category: $scope.article.category
+      }
+
+      $http.put('/articles', data).success(function(data, status) {
+        console.log(status);
+      });
+
+      $location.path('/articles');
+    }
   }])

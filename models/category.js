6f10daca2527c54e4edfd7a47046347deb0d400c
zacscodingclub <zbaston@gmail.com>
2016-09-12 09:40:38 -0400

    add category schema.  add Create and Read getters

--- models/category.js
+++ models/category.js
@@ -1,7 +1,26 @@
 var mongoose = require('mongoose');
 
 var categorySchema = mongoose.Schema({
-
+  name: {
+    type: String,
+    index: true,
+    required: true
+  },
+  description: {
+    type: String
+  }
 });
 
 var Category = module.exports = mongoose.model('Category', categorySchema);
+
+module.exports.getCategories = function(callback) {
+  Category.find(callback);
+}
+
+module.exports.getCategoryById = function(id, callback) {
+  Category.findById(id, callback);
+}
+
+module.exports.createCategory = function(data, callback) {
+  data.save(callback);
+}

197e9a68694afed49a141c8fcf10f585b90e0a03
zacscodingclub <zbaston@gmail.com>
2016-09-12 10:13:00 -0400

    build CUD routes for Article

--- routes/articles.js
+++ routes/articles.js
@@ -27,4 +27,48 @@ router.get('/category/:categoryName', function(req, res, next) {
   })
 })
 
+router.post('/', function(req, res, next) {
+  var newArticle = new Article({
+    title: req.body.title,
+    category: req.body.category,
+    body: req.body.body,
+    url: req.body.url
+  });
+
+  Article.createArticle(newArticle, function(err, article) {
+    if(err) { console.log(err); }
+
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+router.put('/', function(req, res, next) {
+  var data = {
+    id: req.body.id,
+    title: req.body.title,
+    category: req.body.category,
+    body: req.body.body,
+    url: req.body.url
+  }
+
+  Article.updateArticle(data.id, data, function(err, article) {
+    if(err) { console.log(err); }
+
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+
+  Article.removeArticle(id, function(err, article) {
+    if(err) { console.log(err); }
+
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+})
+
 module.exports = router;
